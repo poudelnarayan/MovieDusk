2bed5ad3d4cece728c264ba30852e38ad263bac4
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-01-17 22:50:26 +0545

    Adding filter Bar UI

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -6,6 +6,8 @@ import SearchBar from "./SearchBar";
 const MoviesGrid = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
 
   useEffect(() => {
     fetch("movies.json")
@@ -20,6 +22,27 @@ const MoviesGrid = () => {
   return (
     <div>
       <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select className="filter-dropdown">
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+            <option>Drama</option>
+          </select>
+        </div>
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select className="filter-dropdown">
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
