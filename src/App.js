688ebda07552b6e0aad14b4a2990fb8d352d42a1
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-01-18 19:38:51 +0545

    configuring the router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <MoviesGrid />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Movies</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />} />
+            <Route path="/watchlist" element={<WatchList />} />
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </div>
---
+++ src/components/Watchlist.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css";
+
+const WatchList = () => {
+  return (
+    <div className="watchlist">
+      <h2 className="title">Your Watchlist</h2>
+      <p className="watchlist-text">Your watchlist is empty</p>
+    </div>
+  );
+};
+
+export default WatchList;
